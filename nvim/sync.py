19cdc066e540e1f121612837d0cf4502f633cdb3
bumbledbees <sam@bombus.cloud>
2024-12-20 15:38:06 -0500

    finished implementing push/pull in sync.py
    - moved functionality of push_config() into a new function
      sync_config(), which synchronizes config files between two directories
    - changed push_config() and pull_config() to both use sync_config()
    - added ability to view diff when prompted to overwrite files
    - rewrote confirm() to allow for viewing diffs, renamed to
      confirm_sync() and moved into Neovim class
    - removed log_and_throw(), replaced with raise SyncException and a
      try..except block
    - turned f-strings without placeholders into normal string literals

--- nvim/sync.py
+++ nvim/sync.py
@@ -15,12 +15,14 @@ import sys
 class SyncException(Exception):
     pass
 
+
 class Neovim:
     def __init__(self):
         self.nvim = shutil.which('nvim')
         if not self.nvim:
             raise SyncException('Error: Unable to locate nvim binary')
 
+        self.repo_path = Path(__file__).parent
         self.config_path = Path(self.run('echo stdpath("config") | q',
                                          nvim_args=('--clean',)))
         self.data_path = Path(self.run('echo stdpath("data") | q',
@@ -28,10 +30,10 @@ class Neovim:
         self.packer_path = self.data_path / 'site' / 'pack' / 'packer'
 
         if not self.config_path.exists():
-            logging.info(f'Creating Neovim config directory')
+            logging.info('Creating Neovim config directory')
             self.config_path.mkdir(mode=0o755, parents=True)
         if not self.data_path.exists():
-            logging.info(f'Creating Neovim data directory')
+            logging.info('Creating Neovim data directory')
             self.data_path.mkdir(mode=0o755, parents=True)
 
     def run(self, *cmds, nvim_args=None, ignore_output=False):
@@ -49,22 +51,22 @@ class Neovim:
         output = subprocess.run((self.nvim, *nvim_args, *nvim_cmds),
                                 **subprocess_kwargs)
         if output.returncode != 0:
-            log_and_throw(f'exit code {output.returncode} when running '
-                          f'{self.nvim} {nvim_args} {nvim_cmds}')
+            raise SyncException(f'exit code {output.returncode} when running '
+                                f'{self.nvim} {nvim_args} {nvim_cmds}')
         return output.stdout
 
     def install_packer(self):
-        logging.info(f'Installing packer.nvim...')
+        logging.info('Installing packer.nvim...')
         git_path = shutil.which('git')
         if not git_path:
-            log_and_throw('unable to locate git binary')
+            raise SyncException('unable to locate git binary')
 
         repo_url = 'https://github.com/wbthomason/packer.nvim'
         target_dir = self.packer_path / 'start' / 'packer.nvim'
         git_cmd = ('git', 'clone', '--depth', '1', repo_url, str(target_dir))
         result = subprocess.run(git_cmd, capture_output=True)
         if result.returncode != 0:
-            log_and_throw('error cloning git repo')
+            raise SyncException('error cloning git repo')
 
         # if we run :PackerCompile and :qa in succession, neovim will exit
         # before the first command finishes. if we don't :qa, the process hangs
@@ -81,16 +83,40 @@ class Neovim:
         autoquit = 'autocmd User PackerCompileDone qa'
         self.run(autoquit, 'PackerCompile', ignore_output=True)
 
-    def push_config(self, args):
+    def confirm_sync(self, source_path: Path, target_path: Path) -> bool:
+        print(f'{source_path} differs from {target_path}. ', end='')
+        prompt = 'Overwrite? ([y]es/[n]o/view [d]iff) '
+        while True:
+            choice = input(prompt).lower()
+            if not choice:
+                continue
+            if choice[0] == 'd' or choice == 'view diff':
+                diff = subprocess.run(
+                    ('diff', str(target_path), str(source_path),
+                     '--color=always'), capture_output=True
+                ).stdout
+                subprocess.run(('less',), input=diff, check=True)
+                continue
+            match choice[0]:
+                case 'y':
+                    return True
+                case 'n':
+                    return False
+                case _:
+                    print(f'Invalid choice: {choice}', file=sys.stderr)
+
+    def sync_config(self, source_dir: Path, target_dir: Path,
+                    force: bool = False) -> list[Path]:
         updated_files = []
-        for file_path in Path(__file__).parent.glob('**/*.lua'):
-            target_path = self.config_path / file_path
+        logging.debug(f'sync_config({source_dir=}, {target_dir=}, {force=})')
+        for source_path in source_dir.glob('**/*.lua'):
+            if source_path.name == 'packer_compiled.lua':
+                continue
+            target_path = target_dir / source_path.relative_to(source_dir)
             if target_path.exists():
-                if not filecmp.cmp(file_path, target_path, shallow=False):
-                    prompt = (f'{target_path} differs from {file_path}. '
-                              'Overwrite? (y/n) ')
-                    if args.force or confirm(prompt=prompt):
-                        copyfile(file_path, target_path)
+                if not filecmp.cmp(source_path, target_path, shallow=False):
+                    if force or self.confirm_sync(source_path, target_path):
+                        copyfile(source_path, target_path)
                         updated_files.append(target_path)
                     else:
                         logging.info('Skipping...')
@@ -101,31 +127,26 @@ class Neovim:
                 if not parent_dir.exists():
                     logging.info(f'Creating directories for {target_path}...')
                     parent_dir.mkdir(mode=0o755, parents=True)
-                copyfile(file_path, target_path)
+                copyfile(source_path, target_path)
                 updated_files.append(target_path)
+        return updated_files
 
+    def push_config(self, args):
+        updated_files = self.sync_config(self.repo_path, self.config_path,
+                                         force=args.force)
         if any(p.name == 'plugins.lua' for p in updated_files):
             logging.info('plugins.lua updated, recompiling packer cache...')
             self.packer_compile()
 
     def pull_config(self, args):
-        raise NotImplementedError
+        self.sync_config(self.config_path, self.repo_path, force=args.force)
 
-
-def log_and_throw(msg: str):
-    logging.error(f'Error: {msg}')
-    raise SyncException(msg)
 
 def copyfile(source_path, target_path, *, follow_symlinks=True):
     logging.info(f'Copying {source_path} to {target_path}'
                  f'{" (overwriting)" if target_path.exists() else ""}...')
     shutil.copyfile(source_path, target_path, follow_symlinks=follow_symlinks)
 
-def confirm(prompt='(y/n) ', options='yn'):
-    while not (choice := input(prompt).lower()) or choice[0] not in options:
-        if choice:
-            print(f'Invalid choice: {choice}', file=sys.stderr)
-    return True if choice == 'y' else False
 
 def main():
     nvim = Neovim()
@@ -155,9 +176,15 @@ def main():
     loglevel = logging.ERROR if args.quiet else logging.INFO
     logging.basicConfig(format='%(message)s', level=loglevel,
                         stream=sys.stderr)
-    args.func(args)
+
+    try:
+        args.func(args)
+    except SyncException as ex:
+        print(f'Error: {ex}')
+        return 1
 
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
